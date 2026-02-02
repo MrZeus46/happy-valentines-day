b5a84053d10e6f7ed248cf80169e8aaa0f7926a0
Akhil <akhil.a@gnxsolutions.in>
2026-02-02 22:45:21 +0530

    Updates

--- server/manifest.js
+++ server/manifest.js
@@ -10,10 +10,10 @@ return {
 	assets: new Set(["images/bg-1.png","images/yayyyy.png","robots.txt","sounds/success.mp3"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain",".mp3":"audio/mpeg"},
 	_: {
-		client: {start:"_app/immutable/entry/start.Xm_FvCJp.js",app:"_app/immutable/entry/app.CZSh_Qy_.js",imports:["_app/immutable/entry/start.Xm_FvCJp.js","_app/immutable/chunks/BooE3Ifr.js","_app/immutable/chunks/Dr-ZiH64.js","_app/immutable/chunks/BdSQOy3g.js","_app/immutable/entry/app.CZSh_Qy_.js","_app/immutable/chunks/Dr-ZiH64.js","_app/immutable/chunks/BWpjLrSS.js","_app/immutable/chunks/BcV2ey9D.js","_app/immutable/chunks/BdSQOy3g.js","_app/immutable/chunks/Q4QZ85b4.js","_app/immutable/chunks/CbvhzuuQ.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.DXyRhoPx.js",app:"_app/immutable/entry/app.Da8FJ_2K.js",imports:["_app/immutable/entry/start.DXyRhoPx.js","_app/immutable/chunks/CVcMrlwG.js","_app/immutable/chunks/Dr-ZiH64.js","_app/immutable/chunks/BdSQOy3g.js","_app/immutable/entry/app.Da8FJ_2K.js","_app/immutable/chunks/Dr-ZiH64.js","_app/immutable/chunks/BWpjLrSS.js","_app/immutable/chunks/BcV2ey9D.js","_app/immutable/chunks/BdSQOy3g.js","_app/immutable/chunks/Q4QZ85b4.js","_app/immutable/chunks/CbvhzuuQ.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./chunks/0-B7JUMF-_.js')),
-			__memo(() => import('./chunks/1-D6CTK9DZ.js')),
+			__memo(() => import('./chunks/1-CTPSIObA.js')),
 			__memo(() => import('./chunks/2-BWaqvoaK.js'))
 		],
 		remotes: {
